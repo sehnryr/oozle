8938801d2f2a63a47c1e0fdcf264b1572ff9bd0f
sehnryr <youn@melois.dev>
2023-02-20 22:23:26 +0100

    ffi methods needs to be in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,21 @@
 /// A Rust library for decompressing Kraken, Mermaid, Selkie, Leviathan,
 /// LZNA and Bitknit compressed buffers.
 
-mod ffi;
 mod decompress;
 
 pub use decompress::decompress;
+
+#[cxx::bridge]
+mod ffi {
+    // C++ types and signatures exposed to Rust.
+    unsafe extern "C++" {
+        include!("oozle/include/kraken.h");
+
+        unsafe fn Kraken_Decompress(
+            src: *const u8,
+            src_len: usize,
+            dst: *mut u8,
+            dst_len: usize,
+        ) -> i32;
+    }
+}
