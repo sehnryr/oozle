8e8b9e9f6adb20b61f97faf07f5c337a816a0eeb
sehnryr <youn@melois.dev>
2023-02-20 23:34:09 +0100

    move cpp code in separate folder for cleaner src

--- build.rs
+++ build.rs
@@ -1,15 +1,15 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
-        .file("src/lzna.cpp")
-        .file("src/bitknit.cpp")
-        .file("src/kraken.cpp")
+        .file("src/cpp/lzna.cpp")
+        .file("src/cpp/bitknit.cpp")
+        .file("src/cpp/kraken.cpp")
         .compile("oozle");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
 
-    println!("cargo:rerun-if-changed=src/lzna.cpp");
-    println!("cargo:rerun-if-changed=src/bitknit.cpp");
-    println!("cargo:rerun-if-changed=src/kraken.cpp");
+    println!("cargo:rerun-if-changed=src/cpp/lzna.cpp");
+    println!("cargo:rerun-if-changed=src/cpp/bitknit.cpp");
+    println!("cargo:rerun-if-changed=src/cpp/kraken.cpp");
 
     println!("cargo:rerun-if-changed=include/stdafx.h");
     println!("cargo:rerun-if-changed=include/lzna.h");
