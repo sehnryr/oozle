5459b6fe251c7f2ae01f928eb8aec996dac90602
sehnryr <youn@melois.dev>
2023-02-20 19:41:33 +0100

    create rust lib with cxx
    Use cxx for interoperability between ooz and Rust

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    cxx_build::bridge("src/lib.rs")
+        .file("src/lzna.cpp")
+        .file("src/bitknit.cpp")
+        .file("src/kraken.cpp")
+        .compile("oozle");
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src/lzna.cpp");
+    println!("cargo:rerun-if-changed=src/bitknit.cpp");
+    println!("cargo:rerun-if-changed=src/kraken.cpp");
+    println!("cargo:rerun-if-changed=include/stdafx.h");
+    println!("cargo:rerun-if-changed=include/kraken.h");
+}
---
+++ include/kraken.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "stdafx.h"
+
+int32_t Kraken_Decompress(const u_int8_t *src, size_t src_len, u_int8_t *dst, size_t dst_len);
