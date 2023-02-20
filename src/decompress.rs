01bd10d221e0fc6c912b440ad2c9c167f2f61cad
sehnryr <youn@melois.dev>
2023-02-20 22:13:12 +0100

    separate ffi and decompression method

---
+++ src/decompress.rs
@@ -0,0 +1,28 @@
+pub use crate::ffi::Kraken_Decompress;
+
+/// Decompresses a Kraken, Mermaid, Selkie, Leviathan, LZNA or Bitknit
+/// compressed buffer.
+/// 
+/// # Arguments
+/// 
+/// * `src` - The compressed buffer.
+/// * `dst` - The decompressed buffer.
+/// 
+/// # Returns
+/// 
+/// * `Ok(len)` - The length of the decompressed buffer.
+/// * `Err(())` - The decompression failed.
+pub unsafe fn decompress(src: &[u8], dst: &mut [u8]) -> Result<usize, ()> {
+    let len = ffi::Kraken_Decompress(
+        src.as_ptr(), 
+        src.len(), 
+        dst.as_mut_ptr(), 
+        dst.len(),
+    );
+
+    if len < 0 {
+        Err(())
+    } else {
+        Ok(len as usize)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,44 +3,7 @@
 /// A Rust library for decompressing Kraken, Mermaid, Selkie, Leviathan,
 /// LZNA and Bitknit compressed buffers.
 
-#[cxx::bridge]
-mod ffi {
-    // C++ types and signatures exposed to Rust.
-    unsafe extern "C++" {
-        include!("oozle/include/kraken.h");
+mod ffi;
+mod decompress;
 
-        unsafe fn Kraken_Decompress(
-            src: *const u8,
-            src_len: usize,
-            dst: *mut u8,
-            dst_len: usize,
-        ) -> i32;
-    }
-}
-
-/// Decompresses a Kraken, Mermaid, Selkie, Leviathan, LZNA or Bitknit
-/// compressed buffer.
-/// 
-/// # Arguments
-/// 
-/// * `src` - The compressed buffer.
-/// * `dst` - The decompressed buffer.
-/// 
-/// # Returns
-/// 
-/// * `Ok(len)` - The length of the decompressed buffer.
-/// * `Err(())` - The decompression failed.
-pub unsafe fn decompress(src: &[u8], dst: &mut [u8]) -> Result<usize, ()> {
-    let len = ffi::Kraken_Decompress(
-        src.as_ptr(), 
-        src.len(), 
-        dst.as_mut_ptr(), 
-        dst.len(),
-    );
-
-    if len < 0 {
-        Err(())
-    } else {
-        Ok(len as usize)
-    }
-}
+pub use decompress::decompress;
