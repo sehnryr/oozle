7e3632ffb213d7776ac92c35f5ce3c77e32674ad
sehnryr <youn@melois.dev>
2023-02-21 01:41:21 +0100

    clean code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 /// # oozle
-/// 
+///
 /// A Rust library for decompressing Kraken, Mermaid, Selkie, Leviathan,
 /// LZNA and Bitknit compressed buffers.
-
 mod decompress;
 
 pub use decompress::decompress;
@@ -10,7 +9,7 @@ pub use decompress::decompress;
 #[cxx::bridge]
 mod ffi {
     // C++ types and signatures exposed to Rust.
-    unsafe extern "C++" {
+    extern "C++" {
         include!("oozle/include/decompress.h");
 
         unsafe fn Oozle_Decompress(
